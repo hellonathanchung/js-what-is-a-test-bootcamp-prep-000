970b4b70287050aa65187ab9abd9caec50993297
Nathan Chung <chung.nathanh@gmail.com>
2020-05-28 17:02:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// write your code below!
-var name = "Joe"
-var height = 74
-var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
 
 describe('Name', () => {
     it('returns "Susan"', () => {
